8b4032da45c45cee73e03a13f7a07ad2305a1cd5
JarifHassan <hassanjarif@gmail.com>
2025-05-09 14:24:48 +1200

    Bobo

---
+++ Code.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv("LoanApprovalPrediction.csv")
+
+data.head(5)
+
+obj = (data.dtypes =='object')
+print("categorical variables:", len(list(obj[obj].index)))
+
+data.drop(['Loan_ID'], axis=1,inplace=True)
+
+obj = (data.dtypes == 'object')
+object_cols = list(obj[obj].index)
+plt.figure(figsize=(18,36))
+index = 1
+
+for col in object_cols:
+    y= data[col].value_counts()
+    plt.subplot(11,4,index)
+    plt.xticks(rotation=90)
+    sns.barplot(x=list(y.index), y=y)
+    index +=1
+
+#import label Encoder
+
+from sklearn import preprocessing
+
+#Label_encoder object knows how
+#to understand word labels.
+
+label_encoder = preprocessing.LabelEncoder()
+obj = (data.dtypes == 'object')
+for col in list(obj[obj].index):
+    data[col] = label_encoder.fit_transform(data[col])
+
+obj = (data.dtypes == 'object')
+print('Catagorical variables:', len(list(obj[obj].index)))
