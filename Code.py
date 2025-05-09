ad8017b71e4ab4856bfdd56f78f863b1aaff96b3
JarifHassan <hassanjarif@gmail.com>
2025-05-09 14:31:35 +1200

    Update Code.py

--- Code.py
+++ Code.py
@@ -52,4 +52,20 @@ for col in data.columns:
     data[col] = data[col].fillna(data[col].mean())
 data.isna().sum()
 
+from sklearn.model_selection import train_test_split
+
+X = data.drop(['Loan_Status'], axis=1)
+Y = data['Loan_Status']
+X.shape,Y.shape
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size=0.4, random_state=1)
+X_train.shape, X_test.shape, Y_train.shape, Y_test.shape
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+
+
 
